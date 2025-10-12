6a983b2a6ee954c34eda35065fab80c4718e3a34
Tagir Aliev <noangels.post@gmail.com>
2025-10-12 11:44:38 +0300

    dbworker

--- backend/dbworker.py
+++ backend/dbworker.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 if not logger.handlers:
     logging.basicConfig(level=settings.LOG_LEVEL, format=settings.LOG_FORMAT)
 
+print(settings.SQL_DATABASE_URL) 
 # Асинхронный движок и сессии
 engine = create_async_engine(
     settings.SQL_DATABASE_URL,
